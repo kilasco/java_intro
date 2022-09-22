dc52f1f5113e2ca942dcc4301654cd6c8efc5f16
Jonte Alicea <jonte.alicea@gmail.com>
2022-09-22 19:45:55 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let firstName = "Jonte"
+let lastName = " Alicea"
+let age = 32
+alert("My name is "+ firstName + lastName);
+alert("My age is " + age);
+console.log("So far having a lot of fun! Excited to be challenged. :)");
